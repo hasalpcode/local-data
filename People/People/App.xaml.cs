a0d2794262f3f898ea9b01ac8bfd0920f2e220de
hasalpcode <hasalpcode784@gmail.com>
2024-06-21 00:27:53 +0000

    first commit

--- People/People/App.xaml.cs
+++ People/People/App.xaml.cs
@@ -13,4 +13,16 @@ public partial class App : Application
         // TODO: Initialize the PersonRepository property with the PersonRespository singleton object
 
     }
+
+    public static PersonRepository PersonRepo { get; private set; }
+
+    public App(PersonRepository repo)
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+
+        PersonRepo = repo;
+    }
+}
 }
---
+++ People/People/Modeles/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace People.Modeles
+{
+    [Table("people")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+    }
+}
